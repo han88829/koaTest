8d5d3faa4899411a3b33313cda5ffb29044d5aad
Han <h88829@foxmail.com>
2017-12-26 09:43:26 +0800

    111

---
+++ demo/25.js
@@ -0,0 +1,12 @@
+// MongoDB
+
+
+// 创建链接
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27107/mydb';
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("数据库已创建");
+    db.close();
+})
