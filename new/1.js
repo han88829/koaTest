062556e194d993c69810388dbe29cd89c8f40a53
Han <h88829@foxmail.com>
2018-06-25 22:46:09 +0800

    已修改        new/1.js 已修改        package.json 已修改        yarn.lock

--- new/1.js
+++ new/1.js
@@ -1,8 +1,15 @@
 const koa = require('koa');
 
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new koa();
+
+app.use(bodyParser({
+    onerror: function (err, ctx) {
+        ctx.throw('body parse error', 422);
+    }
+}));
 
 router.get('/', async (ctx) => {
     ctx.body = "你好 koa!";
@@ -16,6 +23,33 @@ router.get('/test/:id', async (ctx) => {
     ctx.body = `你传入的id是${data.id}`;
 })
 
+router.post('/list', async (ctx) => {
+    try {
+        let data = await ctx.request.body;
+        let str = '';
+        ctx.req.on('data', (chunk) => {
+            str += chunk;
+        })
+        ctx.req.on('end', () => {
+            console.log(JSON.parse(str));
+            ctx.body = str;
+        })
+        /*
+        *   headers: {
+        *        "Content-Type":"application/json"
+        *    },
+        *   前端请求header的content-type一定要设置对，否则会接收不到数据，
+        */
+
+    } catch (error) {
+        console.log(error)
+        ctx.body = 'error';
+
+    }
+
+})
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3001);
+
